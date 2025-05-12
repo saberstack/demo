9d40fc6a2fa4f83e9192a1a5a13d4d1b1895cdf4
Rangel Spasov <raspasov@gmail.com>
2025-05-12 13:12:50 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import {npmDeps} from "./target/public/cljs-out/web_rn/npm_deps.js";
-import {assets} from "./target/public/cljs-out/web/krell_assets.js";
-import {krellNpmDeps} from "./target/public/cljs-out/web/krell_npm_deps.js";
-var options = {optionsUrl: "http://localhost:9500/cljs-out/web/cljsc_opts.json",
+import {npmDeps} from "./target/public/cljs-out/web-dev_rn/npm_deps.js";
+import {assets} from "./target/public/cljs-out/web-dev/krell_assets.js";
+import {krellNpmDeps} from "./target/public/cljs-out/web-dev/krell_npm_deps.js";
+var options = {optionsUrl: "http://localhost:9500/cljs-out/web-dev/cljsc_opts.json",
                autoRefresh: true};
-var figBridge = require("./target/public/cljs-out/web_rn/figwheel-bridge.js");
+var figBridge = require("./target/public/cljs-out/web-dev_rn/figwheel-bridge.js");
 figBridge.shimRequire({...assets, ...krellNpmDeps, ...npmDeps});
 import { registerRootComponent } from 'expo';
 registerRootComponent(figBridge.createBridgeComponent(options));
